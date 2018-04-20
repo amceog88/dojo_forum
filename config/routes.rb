cb2bdb6bb3b61e5b53e9206d56e060e79c1feedd
Siegmund <amceog88@gmail.com>
2018-04-20 15:44:49 +0800

    add back-end controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "posts#index" 
+
+
+  namespace :admin do
+
+
+    resources :posts
+    resources :users
+
+    root "posts#index"
+  end
 end
